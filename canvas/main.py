5c28cf78ebd4d1873df502400336e47c5d77a33c
Spencer <sthompson.dev@outlook.com>
2024-01-27 00:01:40 -0700

    its working :)

--- canvas/main.py
+++ canvas/main.py
@@ -1,1 +1,22 @@
-"""Docstring just to push"""
+"""
+Docstring just to push
+"""
+
+import requests
+import os
+
+def courses(enrollment_state = 'active'):
+    token = os.getenv('MY_CANVAS_TOKEN')
+    url = 'https://uvu.instructure.com/api/v1/courses'
+
+    r = requests.get(url+'?enrollment_state='+enrollment_state, headers = {'Authorization': 'Bearer ' + str( token )})
+
+    return r.json()
+
+def main():
+    # print(courses())
+    for course in courses():
+        print(course['name'])
+
+if __name__ == "__main__":
+    main()
