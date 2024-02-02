2b813a0c97aa239f197f05a626b97b92019f67f3
Samuel Griffey <samuelgriffey0@gmail.com>
2024-02-02 16:40:19 -0700

    first commit from vscode

--- canvas/main.py
+++ canvas/main.py
@@ -13,10 +13,23 @@ def courses(enrollment_state = 'active'):
 
     return r.json()
 
+def assignments(course_id, order_by = 'due_at'):
+    token = os.getenv('MY_CANVAS_TOKEN')
+    url = 'https://uvu.instructure.com/api/v1/courses'
+    url += f'/{course_id}/assignments'
+
+    r = requests.get(url+'?order_by='+order_by, headers = {'Authorization': 'Bearer ' + str( token )})
+
+    return r.json()
+
 def main():
     # print(courses())
     for course in courses():
-        print(course['name'])
+        print(f"{course['name']} -- COURSE ID:  {course['id']}")
+    course_id = input("Input an ID to see assignments: ")
+    for assignment in assignments(course_id):
+        print(f"{assignment['name']} -- DUE DATE:  {assignment['due_at']}")
+
 
 if __name__ == "__main__":
     main()
