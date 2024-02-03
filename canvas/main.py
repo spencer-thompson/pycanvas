a335b55cab0a2843523fa071a304cbaa0506d715
TKD498 <driggskingtate@gmail.com>
2024-02-03 02:51:21 -0700

    feat: setup CLI boilerplate

--- canvas/main.py
+++ canvas/main.py
@@ -2,21 +2,31 @@
 Docstring just to push
 """
 
+import click
 import requests
 import os
 
-def courses(enrollment_state = 'active'):
-    token = os.getenv('MY_CANVAS_TOKEN')
-    url = 'https://uvu.instructure.com/api/v1/courses'
 
-    r = requests.get(url+'?enrollment_state='+enrollment_state, headers = {'Authorization': 'Bearer ' + str( token )})
-
-    return r.json()
-
+@click.group()
 def main():
-    # print(courses())
-    for course in courses():
-        print(course['name'])
+    """Canvas CLI Tool"""
+    pass
+
+
+@main.command()
+@click.option("--enrollmentstate", default="active", help="An example parameter.")
+def courses(enrollmentstate):
+    """This sub command runs all interfaces related to courses"""
+    token = os.getenv("MY_CANVAS_TOKEN")
+    url = "https://uvu.instructure.com/api/v1/courses"
+    r = requests.get(
+        url,
+        params={"enrollment_state": enrollmentstate},
+        headers={"Authorization": "Bearer " + str(token)},
+    )
+    for course in r.json():
+        click.echo(course["name"])
+
 
 if __name__ == "__main__":
     main()
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="canvas",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",  # Add other dependencies here
+    ],
+    entry_points="""
+        [console_scripts]
+        canvas=canvas.main:main
+    """,
+)
